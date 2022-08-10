f8175865f66ddec0c8a325b6e2f7af8f800a7884
Michelle_Anguiano <Michelle.Anguiano@nbcuni.com>
2022-08-10 00:30:17 -0500

    - add Unit Test for authorization.js

--- authorization-service/test/lambda.authorization.test.js
+++ authorization-service/test/lambda.authorization.test.js
@@ -8,11 +8,39 @@ const basic = require('../authorization').basicAuthorizer;
 describe('Authorization Test', () => {
     describe('lambda', () => {
         describe('authorization', () => {
-            it('test', async () => {
+            it('Should get Allow Effect', async () => {
                 const result = await lambda(basic)
-                    .event({})
+                    .event({
+                        "type": "TOKEN",
+                        "methodArn": "arn:aws:execute-api:us-east-1:436988374415:guouzuadzd/dev/GET/import/Alexa",
+                        "authorizationToken": "Basic bWFuZ3VpYW5vZXBhbT1URVNUX1BBU1NXT1JE"
+                    })
                     .expectResult((data) => data);
+
+                expect(result.policyDocument.Statement[0].Effect).equals('Allow');
             });
+
+            it('Should get Allow Unauthorized', async () => {
+                const result = await lambda(basic)
+                    .event({
+                        })
+                    .expectResult((data) => data);
+
+                expect(result.policyDocument.Statement[0].Effect).equals('Unauthorized');
+            });
+
+            it('Should get Deny Effect', async () => {
+                const result = await lambda(basic)
+                    .event({
+                        "type": "TOKEN",
+                        "methodArn": "arn:aws:execute-api:us-east-1:436988374415:guouzuadzd/dev/GET/import/Alexa",
+                        "authorizationToken": "Basic d2RIM2ZQREwwdkVDVW1xQ3ZDdGk="
+                    })
+                    .expectResult((data) => data);
+
+                expect(result.policyDocument.Statement[0].Effect).equals('Deny');
+            });
+
         });
     })
 });
